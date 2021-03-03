00ee6dfb78b611ea2f207aeb1830bfc82d8c69e1
evkrem <evkremx@gmail.com>
2021-03-03 11:44:19 +0200

    принудительно уменьшил время ожидания загрузки страниц в случае, если какой-либо из элементов тормозит полную загрузку страницы

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,16 +5,29 @@ from .locators import SafeModeLocators
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.maximize_window()
-        self.browser.get(self.url)
+        # self.browser.get(self.url)
+
+        self.browser.set_page_load_timeout(3)
+        try:
+            self.browser.get(self.url)
+            print("URL successfully Accessed")
+            # self.browser.quit()
+        except TimeoutException as e:
+            print("Page load Timeout Occured. Quiting !!!")
+            # self.browser.quit()
+
+
         try:
             self.browser.find_element(*SafeModeLocators.BUTTON_FOR_ENTRANCE_IN_SAFE_MODE).click()
             self.browser.find_element(*SafeModeLocators.BUTTON_FOR_ENTRANCE_ON_SITE).click()
         except NoSuchElementException:
             return
+
+
--- pages/blog_page.py
+++ pages/blog_page.py
@@ -5,8 +5,8 @@ import time
 class BlogPage(BasePage):
     def check_existence_articles(self,browser):
         print("\nvsego statey = ",len(self.browser.find_elements(*BlogPagesLocators.NAME_ARTICLE)))
-        assert len(self.browser.find_elements(*BlogPagesLocators.NAME_ARTICLE)) < 1,"articles is not"
+        assert len(self.browser.find_elements(*BlogPagesLocators.NAME_ARTICLE)) > 1,"articles is not"
 
     def check_existence_img(self,browser):
-        assert len(self.browser.find_elements(*BlogPagesLocators.IMG)) != len(self.browser.find_elements(
+        assert len(self.browser.find_elements(*BlogPagesLocators.IMG)) == len(self.browser.find_elements(
             *BlogPagesLocators.NAME_ARTICLE)),"number pictures isn`t equally number articles"
